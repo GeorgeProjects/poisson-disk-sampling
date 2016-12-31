0a93e9cbd262dd93ad854c293381703e584a4a48
kchapelier <kevin.chapelier@gmail.com>
2016-12-31 13:37:36 +0100

    very rough, mostly broken, not backward compatible, distance function implementation

--- eyeball.js
+++ eyeball.js
@@ -2,43 +2,30 @@
 
 var Poisson = require('./'),
     save = require('save-pixels'),
-    zeros = require('zeros');
+    zeros = require('zeros'),
+    Noise = require('noisejs').Noise;
 
-var dimensions = process.argv.length > 2 ? parseInt(process.argv[2], 10) : 2;
+var noise = new Noise();
 
-if (dimensions === 3) {
-    var outputPng = function (sampling) {
-        var array = zeros([sampling.shape[0], sampling.shape[1]], 'float32');
+var outputPng = function (sampling) {
+    var array = zeros([sampling.shape[0], sampling.shape[1]], 'float32');
+    var i = 0;
 
-        sampling.samplePoints.forEach(function (point) {
-            array.set(Math.round(point[0]), Math.round(point[1]), 255 * Math.pow((1 + point[2]) / sampling.shape[2], 2));
-        });
+    sampling.samplePoints.forEach(function (point) {
+        i++;
+        array.set(Math.round(point[0]), Math.round(point[1]), 255);
+    });
 
-        save(array, 'png').pipe(process.stdout);
-    };
+    save(array, 'png').pipe(process.stdout);
+};
 
-    var p = new Poisson([800, 400, 200], 20, 25, 10);
-    p.fill();
+var p = new Poisson([900, 400], 1.25, 7.5, 25, function (point) {
+    return Math.pow(0.5 + noise.perlin2(point[0] / 75, point[1] / 75) * 0.5, 2.25);
+});
 
-    outputPng(p);
-} else {
-    var outputPng = function (sampling) {
-        var array = zeros([sampling.shape[0], sampling.shape[1]], 'float32');
-        var i = 0;
+p.fill();
 
-        sampling.samplePoints.forEach(function (point) {
-            i++;
-            array.set(Math.round(point[0]), Math.round(point[1]), 50 + (Math.cos(i / 50) + 1) * 205 / 2 | 0 /*(50 + (i % 600) / 3 | 0)*/);
-        });
-
-        save(array, 'png').pipe(process.stdout);
-    };
-
-    var p = new Poisson([900, 400], 8, 8, 20);
-    p.fill();
-
-    outputPng(p);
-}
+outputPng(p);
 
 
 
